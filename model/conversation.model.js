30a1eb0b12188bdcf082519e2a86ee75a44332b5
Mohd-Akbar1 <rizvi@LAPTOP-OPORSGN9.localdomain>
2025-07-01 18:20:48 +0000

    making models

---
+++ model/conversation.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const conversationSchema=mongoose.Schema({
+   paticipants:[{type:mongoose.Schema.Types.ObjectId,ref:'user'}],
+   message:{type:mongoose.Schema.Types.ObjectId,ref:'message',default:[]},
+},{timestamps:true})
+
+const conversation=mongoose.model('conversation',conversationSchema)
+export default conversation
